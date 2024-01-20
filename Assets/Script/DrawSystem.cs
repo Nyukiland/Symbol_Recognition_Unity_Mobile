3f30a834d9e5382252a45b93f2a1e49e3b40239c
Nyukiland <roman211113@gmail.com>
2024-01-20 23:14:17 +0100

    first few baby step

--- Assets/Script/DrawSystem.cs
+++ Assets/Script/DrawSystem.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DrawSystem : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 lineOffset = new Vector3(0, 0, 1);
+
     LineRenderer line;
-    List<Vector2> linePos;
+    List<Vector3> linePos = new();
 
     void Start()
     {
-        line = new GameObject().AddComponent<LineRenderer>();
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 0;
     }
 
     // Update is called once per frame
@@ -22,18 +27,47 @@ public class DrawSystem : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
+                    StartRecord(Camera.main.ScreenToWorldPoint(touch.position) + lineOffset);
                     break;
                 case TouchPhase.Moved:
+                    ContinueRecord(Camera.main.ScreenToWorldPoint(touch.position) + lineOffset);
                     break;
                 case TouchPhase.Ended:
+                    StartCoroutine(EndRecordChecking(linePos));
                     break;
 
             }
         }
     }
 
-    void StartRecord()
+    void StartRecord(Vector3 pos)
     {
-        //line.
+        StopAllCoroutines();
+
+        line.positionCount = 0;
+        linePos.Clear();
+        linePos.Add(pos);
+    }
+
+    void ContinueRecord(Vector3 pos)
+    {
+        linePos.Add(pos);
+
+        Debug.Log(Vector3.Angle(linePos[linePos.Count - 2], linePos[linePos.Count - 1])); //doing it wrong, must create a direction and compare it to a reference vector
+        Debug.Log("-----------");
+
+        line.positionCount = linePos.Count;
+        line.SetPositions(linePos.ToArray());
+    }
+
+    IEnumerator EndRecordChecking(List<Vector3> allPos)
+    {
+        Vector3[] temp = allPos.ToArray();
+        temp = GetComponent<symbolRecognition>().SymplifyPoints(temp);
+
+        while (allPos.Count != temp.Length) yield return null;
+
+        GetComponent<symbolRecognition>().RecognitionSystem(temp);
+
     }
 }
--- Assets/Script/symbolRecognition.cs
+++ Assets/Script/symbolRecognition.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class symbolRecognition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3[] SymplifyPoints(Vector3[] allPoints)
     {
-        
+        //compare angles direction with three points and and remove points step by step
+        // maybe do a pos check first to go faster (remove all overlapping points or points too close to each other)
+
+        return allPoints;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RecognitionSystem(Vector3[] allPoints)
     {
-        
+        //use distance and angle to compare shape
+        //compare one by one the points with the points of the database and narrow it down
+
+        //need to do a function to record symbol
     }
 }
